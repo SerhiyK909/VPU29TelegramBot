9fa5e0965ca779361c84da8c9a2b447f2a07f76d
Serhiy Kuznecov <sergijkuznecov656@gmail.com>
2024-04-04 15:26:49 +0300

    Inplemented location telegramBot

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
-import  os
+import os
 import logging
-
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from dotenv import load_dotenv
-
-load_dotenv()  # take environment variables from .env.
-
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext  import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+load_dotenv()
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-
-
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    keyboard = [
+        [KeyboardButton('Share my location', request_location=True)],
+        ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Im a bot, please talk to me!',
+        reply_markup=reply_markup
+    )
 
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def location(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    lat = update.message.location.latitude
+    lon = update.message.location.longitude
+    await update.message.reply_text(f'lat = {lat}, lon = {lon}')
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+    hello_handler = CommandHandler('hello', hello)
+    application.add_handler(hello_handler)
+    location_handler = MessageHandler(filters.LOCATION, location)
+    application.add_handler(location_handler)
 
     application.run_polling()
